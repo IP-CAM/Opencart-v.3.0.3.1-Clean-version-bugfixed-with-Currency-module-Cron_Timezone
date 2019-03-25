a9a721b5ac93308f2142d99ae3247183deeae9a1
condor2 <condor20_05@yahoo.it>
2019-03-25 14:31:53 +0200

    ebay listing fix
    https://forum.opencart.com/viewtopic.php?f=201&t=210339

--- upload/catalog/controller/extension/module/ebay_listing.php
+++ upload/catalog/controller/extension/module/ebay_listing.php
@@ -1,7 +1,7 @@
 <?php
 class ControllerExtensionModuleEbayListing extends Controller {
 	public function index() {
-		if ($this->config->get('ebay_status') == 1) {
+		if ($this->config->get('module_ebay_status') == 1) {
 			$this->load->language('extension/module/ebay');
 			
 			$this->load->model('tool/image');
@@ -11,7 +11,7 @@ class ControllerExtensionModuleEbayListing extends Controller {
 
 			$data['products'] = array();
 
-			$products = $this->cache->get('ebay_listing.' . md5(serialize($products)));
+			$products = $this->cache->get('module_ebay_listing.' . md5(serialize($products)));
 
 			if (!$products) {
 				$products = $this->model_extension_openbay_ebay_product->getDisplayProducts();
@@ -21,9 +21,9 @@ class ControllerExtensionModuleEbayListing extends Controller {
 
 			foreach($products['products'] as $product) {
 				if (isset($product['pictures'][0])) {
-					$image = $this->model_extension_openbay_ebay_product->resize($product['pictures'][0], $this->config->get('ebay_listing_width'), $this->config->get('ebay_listing_height'));
+					$image = $this->model_extension_openbay_ebay_product->resize($product['pictures'][0], $this->config->get('module_ebay_listing_width'), $this->config->get('module_ebay_listing_height'));
 				} else {
-					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('ebay_listing_width'), $this->config->get('ebay_listing_height'));
+					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('module_ebay_listing_width'), $this->config->get('module_ebay_listing_height'));
 				}
 
 				$data['products'][] = array(

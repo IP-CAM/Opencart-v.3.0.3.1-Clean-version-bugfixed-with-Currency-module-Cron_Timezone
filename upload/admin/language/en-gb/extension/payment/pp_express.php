866943ccefd568d8204906510525d3a6e09655af
condor2 <condor20_05@yahoo.it>
2019-07-14 14:02:42 +0300

    Update pp_express.php
    https://forum.opencart.com/viewtopic.php?f=201&t=210578

--- upload/admin/language/en-gb/extension/payment/pp_express.php
+++ upload/admin/language/en-gb/extension/payment/pp_express.php
@@ -1,12 +1,11 @@
 <?php
 // Heading
 $_['heading_title']					 = 'PayPal Express Checkout';
-
 // Text
 $_['text_extension']				 = 'Extensions';
 $_['text_success']				 	 = 'Success: You have modified PayPal Express Checkout account details!';
 $_['text_edit']                      = 'Edit PayPal Express Checkout';
-$_['text_pp_express']				 = '<a target="_BLANK" href="https://www.paypal.com/uk/mrb/pal=V4T754QB63XXL"><img src="view/image/payment/paypal.png" alt="PayPal Express Checkout" title="PayPal Express Checkout" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_pp_express']				 = '<a target="_BLANK" href="https://www.paypal.com/uk/merchantsignup/applicationChecklist?utm_source=A_partner_UK_201812_X_B2B_OpenCart_SMB_gb_EN&utm_medium=web&pal=V4T754QB63XXL"><img src="view/image/payment/paypal.png" alt="PayPal Express Checkout" title="PayPal Express Checkout" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_authorization']			 = 'Authorization';
 $_['text_sale']						 = 'Sale';
 $_['text_signup']                    = 'Sign up for PayPal - save your settings first as this page will be refreshed';
@@ -18,28 +17,17 @@ $_['text_show_quick_setup']          = 'Show Quick Setup';
 $_['text_quick_setup']             	 = 'Quick setup - Link an existing or create a new PayPal account to start accepting payments in minutes';
 $_['text_paypal_consent']		 	 = 'By using the quick setup tool you allow PayPal to receive information about your store';
 $_['text_success_connect']			 = 'Success: You have connected your PayPal account!';
-$_['text_promotion_main']		 	 = 'Give your buyers a simplified checkout experience on multiple devices that keeps them local to your website throughout the payment process';
-$_['text_learn_more']			 	 = '(Learn more)';
-$_['text_promotion_li_1']			 = 'Start accepting PayPal in three clicks';
-$_['text_promotion_li_2']			 = 'Accept payments from around the world';
-$_['text_promotion_li_3']			 = 'Offer Express Checkout Shortcut, let buyers checkout directly from your basket page';
 $_['text_connect_paypal']			 = 'Connect with PayPal';
+$_['text_incontext_not_supported']	 = '* Not supported with In-Context Checkout';																				 
 $_['text_retrieve']	 				 = 'Your details have been entered from PayPal';
 $_['text_enable_button']			 = 'We recommend offering PayPal Express Shortcut to maximise checkout conversion, this allows customers to use their PayPal address book and <strong>checkout is as little as three taps</strong> from the basket page. Click enable to install the extension and access the layout manager, you will ned to add "PayPal Express Checkout Button" to the checkout layout';
-$_['text_horizontal']	 			 = 'Horizontal';
-$_['text_vertical']	 			 	 = 'Vertical';
-$_['text_medium']	 			 	 = 'Medium';
-$_['text_large']	 			 	 = 'Large';
-$_['text_responsive']	 			 = 'Responsive';
-$_['text_pill']	 			 		 = 'Pill';
-$_['text_rectangle']	 			 = 'Rectangle';
-$_['text_gold']	 			 		 = 'Gold';
-$_['text_blue']	 			 		 = 'Blue';
-$_['text_silver']	 			 	 = 'Silver';
-$_['text_black']	 			 	 = 'Black';
-$_['text_button_styling']	 		 = 'Button Styling';
-$_['text_funding_type']			 	 = 'Optional Funding Methods';
-
+$_['text_error']					 = 'The transaction ID to initiate a refund could not be found!';
+$_['text_paypal_zones_info']		 = 'For more information about PayPal Express Checkout zones, please refer to the <a href="https://developer.paypal.com/docs/classic/api/state_codes/" target="_blank">official web page</a>'; 
+$_['text_country']				 	 = 'Country Name';
+$_['text_zone']						 = 'Zone Name';
+$_['text_zone_code']				 = 'Store Zone Code';
+$_['text_paypal_zone_code']			 = 'Paypal Zone Code';
+$_['text_paypal_ship_to_state']		 = 'Ship To State';
 // Entry
 $_['entry_username']				 = 'API Username';
 $_['entry_password']				 = 'API Password';
@@ -67,29 +55,21 @@ $_['entry_processed_status']		 = 'Processed Status';
 $_['entry_refunded_status']			 = 'Refunded Status';
 $_['entry_reversed_status']			 = 'Reversed Status';
 $_['entry_voided_status']			 = 'Voided Status';
+$_['entry_allow_notes']				 = 'Allow Notes';
+$_['entry_colour']	      			 = 'Page Background Colour';											
 $_['entry_logo']					 = 'Logo';
-$_['entry_style_layout']			 = 'Button Layout';
-$_['entry_style_size']			 	 = 'Button Size';
-$_['entry_style_shape']			 	 = 'Button Shape';
-$_['entry_style_color']			 	 = 'Button Colour';
-$_['entry_pp_credit']	 			 = 'PayPal Credit';
-$_['entry_pp_cards']	 			 = 'Credit or Debit Cards';
-$_['entry_pp_elv']	 			 	 = 'DE (German) bank payment';
-
+$_['entry_incontext']				 = 'Disable In-Context Checkout';
 // Tab
 $_['tab_api']				         = 'API Details';
 $_['tab_order_status']				 = 'Order status';
 $_['tab_checkout']					 = 'Checkout';
-
+$_['tab_zone']						 = 'Unmatched Zones';
 // Help
 $_['help_ipn']						 = 'Required for subscriptions';
 $_['help_total']					 = 'The checkout total the order must reach before this payment method becomes active';
 $_['help_logo']						 = 'Max 750px(w) x 90px(h)<br />You should only use a logo if you have SSL set up.';
+$_['help_colour']					 = '6 character HTML colour code';														
 $_['help_currency']					 = 'Used for transaction searches';
-$_['help_pp_credit']				 = 'Non-US merchants need to apply to accept PayPal Credit, contact PayPal for more information. Enabled by default for all US buyers';
-$_['help_pp_cards']				 	 = 'Enabled by default for all buyers';
-$_['help_pp_elv']				 	 = 'Elektronisches Lastschriftverfahren (bank payments). Enabled by default for all DE buyers';
-
 // Error
 $_['error_permission']				 = 'Warning: You do not have permission to modify payment PayPal Express Checkout!';
 $_['error_username']				 = 'API Username Required!';
@@ -101,3 +81,6 @@ $_['error_sandbox_signature']		 = 'API Sandbox Signature Required!';
 $_['error_api']						 = 'Paypal Authorization Error';
 $_['error_api_sandbox']				 = 'Paypal Sandbox Authorization Error';
 $_['error_consent']				 	 = 'To use quick setup you need to permit PayPal to use your store information';
+$_['error_start_date']				 = 'Enter a start date';
+$_['error_merchant_id']				 = 'The merchant ID could not be found!';
+$_['error_helper']					 = 'There helper file has not been uploaded!';

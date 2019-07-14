7dd5a6a138eadce6e4eac19ee95a8b70e16e496b
condor2 <condor20_05@yahoo.it>
2019-07-14 14:03:19 +0300

    Update pp_express_order.php
    https://forum.opencart.com/viewtopic.php?f=201&t=210578

--- upload/admin/language/en-gb/extension/payment/pp_express_order.php
+++ upload/admin/language/en-gb/extension/payment/pp_express_order.php
@@ -16,7 +16,6 @@ $_['text_full_refund']		 = 'Full refund';
 $_['text_partial_refund']	 = 'Partial refund';
 $_['text_payment']		 	 = 'Payment';
 $_['text_current_refunds']   = 'Refunds have already been done for this transaction. The max refund is';
-
 // Column
 $_['column_transaction']	 = 'Transaction ID';
 $_['column_amount']			 = 'Amount';
@@ -25,26 +24,22 @@ $_['column_status']			 = 'Status';
 $_['column_pending_reason']	 = 'Pending Reason';
 $_['column_date_added']		 = 'Date Added';
 $_['column_action']			 = 'Action';
-
 // Entry
 $_['entry_capture_amount']	 = 'Capture amount';
 $_['entry_capture_complete'] = 'Complete capture';
 $_['entry_full_refund']		 = 'Full refund';
 $_['entry_amount']			 = 'Amount';
 $_['entry_note']             = 'Note';
-
 // Help
 $_['help_capture_complete']  = 'If this is a the last capture.';
-
 // Tab
 $_['tab_capture']		     = 'Capture';
 $_['tab_refund']             = 'Refund';
-
 // Button
 $_['button_void']			 = 'Void';
 $_['button_capture']		 = 'Capture';
 $_['button_refund']		     = 'Issue refund';
-
+$_['button_resend']			 = 'Resend';								  
 // Error
 $_['error_capture']		     = 'Enter an amount to capture';
 $_['error_transaction']	     = 'Transaction could not be carried out!';
